7c7addc93c11f4ac7d55a682bae8c0b430686f09
Dody Prasetyo <prasetyodody17@gmail.com>
2017-07-23 20:24:31 +0800

    Fixed bug plugin not found in tinymce editor admin

--- resources/views/dashboard/artikel/create.blade.php
+++ resources/views/dashboard/artikel/create.blade.php
@@ -113,7 +113,7 @@
       'advlist autolink lists link image charmap print preview hr anchor pagebreak',
       'searchreplace wordcount visualblocks visualchars code fullscreen',
       'insertdatetime media nonbreaking save table contextmenu directionality',
-      'emoticons template paste textcolor colorpicker textpattern image imagetools codesample toc responsivefilemanager'
+      'emoticons template paste textcolor colorpicker textpattern image imagetools codesample toc'
     ],
     toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
     toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
--- resources/views/dashboard/artikel/edit.blade.php
+++ resources/views/dashboard/artikel/edit.blade.php
@@ -83,7 +83,7 @@
       'advlist autolink lists link image charmap print preview hr anchor pagebreak',
       'searchreplace wordcount visualblocks visualchars code fullscreen',
       'insertdatetime media nonbreaking save table contextmenu directionality',
-      'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc responsivefilemanager'
+      'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
     ],
     toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
     toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
