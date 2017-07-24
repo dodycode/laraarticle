0294ab96210269b3cc25151d9ded29975c7b3505
Dody Prasetyo <prasetyodody17@gmail.com>
2017-07-24 09:44:14 +0800

    added force https url

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
-        Petehouston\Tinymce\TinymceServiceProvider::class,
 
         /*
          * Application Service Providers...
