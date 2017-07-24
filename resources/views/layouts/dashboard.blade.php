69b72702be841ef225e24a5f4f0ec7acdae643ed
Dody Prasetyo <prasetyodody17@gmail.com>
2017-07-24 09:48:22 +0800

    Fixed typos bug

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="{{ secure_secure_asset('images/favico-64x64.png') }}" type="image/x-icon" />
-    <link rel="icon" href="{{ secure_secure_asset('images/favico-64x64.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ secure_asset('images/favico-64x64.png') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ secure_asset('images/favico-64x64.png') }}" type="image/x-icon">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
